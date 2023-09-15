d184c4df2deedcedd85ae3a578992383b9c34a51
Thiago <thiagolos1999@gmail.com>
2023-09-15 10:28:37 +0100

    feat: indicator category added

--- client/src/components/indicatorCard.jsx
+++ client/src/components/indicatorCard.jsx
@@ -4,15 +4,18 @@ export default function IndicatorCard({ indicator }) {
   return (
     <>
       <div className="indicator-container">
-        <h2>{indicator.Category}</h2>
+        <div className="header-and-category">
+          <h2>{indicator.Category}</h2>
+          <em>Category: {indicator.CategoryGroup}</em>
+        </div>
         <span>
           {indicator.LatestValue}{" "}
           {indicator.Unit && indicator.Frequency ? (
             <>
-              {indicator.Unit} / <em>Updated {indicator.Frequency}</em>
+              {indicator.Unit} <em>/ Updated {indicator.Frequency}</em>
             </>
           ) : (
-            indicator.Frequency
+            <em>/ Updated {indicator.Frequency}</em>
           )}
         </span>
       </div>
